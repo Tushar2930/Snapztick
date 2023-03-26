a20cf0efd6253a27d74495f2bff234a903978edd
Tushar <tusharshingane1234@gmail.com>
2023-03-26 11:09:43 +0530

    temp

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar(){
     <div class="px-2 pt-2 pb-3 space-y-1">
       <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
       <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
+      <a href="/form" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
     </div>
   </div>
 </nav>
